e41effb4b23fd21c7e31341e7f87f1e63c5bfa60
Buckhouse <james.buckhouse@gmail.com>
2018-08-31 16:17:44 -0700

    additional files first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'James Buckhouse',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learning to use React</h1>
+    <p>Using Meng To's fasntastic tutorial</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
